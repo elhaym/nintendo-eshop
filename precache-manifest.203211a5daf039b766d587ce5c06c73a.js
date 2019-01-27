3ebb732819156ee87099e811af49e76262fdd3f2
Elhaym <elhaym@users.noreply.github.com>
2019-01-28 00:21:57 +0100

    Updates

---
+++ precache-manifest.203211a5daf039b766d587ce5c06c73a.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = [
+  {
+    "revision": "29acdd2fcab673118268",
+    "url": "/nintendo-eshop/static/css/main.913bebb8.chunk.css"
+  },
+  {
+    "revision": "29acdd2fcab673118268",
+    "url": "/nintendo-eshop/static/js/main.29acdd2f.chunk.js"
+  },
+  {
+    "revision": "e717ebad348aa3a8d8c8",
+    "url": "/nintendo-eshop/static/css/1.54685762.chunk.css"
+  },
+  {
+    "revision": "e717ebad348aa3a8d8c8",
+    "url": "/nintendo-eshop/static/js/1.e717ebad.chunk.js"
+  },
+  {
+    "revision": "7a0f5f82449de2319c4a",
+    "url": "/nintendo-eshop/static/js/runtime~main.7a0f5f82.js"
+  },
+  {
+    "revision": "1c4e47a2eff10a109e3dac515ac67b85",
+    "url": "/nintendo-eshop/static/media/logo.1c4e47a2.png"
+  },
+  {
+    "revision": "6b72ff0582ea198e3688ee59a7ae40a7",
+    "url": "/nintendo-eshop/static/media/online_switch_poster.6b72ff05.png"
+  },
+  {
+    "revision": "c4e53851d7f7acdae2340b79f8200e1b",
+    "url": "/nintendo-eshop/static/media/bgi.c4e53851.jpg"
+  },
+  {
+    "revision": "8d62533598b1fe4e78d90a43db3dabdf",
+    "url": "/nintendo-eshop/static/media/bgx.8d625335.jpg"
+  },
+  {
+    "revision": "23c365ee68bfe094d8b28a4645894b90",
+    "url": "/nintendo-eshop/static/media/bgs.23c365ee.jpg"
+  },
+  {
+    "revision": "0b4a864f5d5f974b2734603c6e1c454e",
+    "url": "/nintendo-eshop/static/media/tv01.0b4a864f.jpg"
+  },
+  {
+    "revision": "a3467e50eef66ddf9228949c13f0aa43",
+    "url": "/nintendo-eshop/static/media/table01.a3467e50.jpg"
+  },
+  {
+    "revision": "ccc3914398bfac43d1d99978a69034f8",
+    "url": "/nintendo-eshop/static/media/hh01.ccc39143.jpg"
+  },
+  {
+    "revision": "33566f4e895d08f822513249871cee70",
+    "url": "/nintendo-eshop/static/media/notv.33566f4e.jpg"
+  },
+  {
+    "revision": "6591a062a5a3f6047bdb4406ec417f58",
+    "url": "/nintendo-eshop/static/media/pokego2.6591a062.jpg"
+  },
+  {
+    "revision": "46183950ce8847fd64795fa9adf519c7",
+    "url": "/nintendo-eshop/static/media/octo2.46183950.jpg"
+  },
+  {
+    "revision": "04c321acccf3a6cf9451426f36e131db",
+    "url": "/nintendo-eshop/static/media/ssbu-col.04c321ac.jpg"
+  },
+  {
+    "revision": "43fb033d843db1736f4d834a86027250",
+    "url": "/nintendo-eshop/static/media/bg-fire-emblem.43fb033d.png"
+  },
+  {
+    "revision": "9ce4b97544d4d40abf89bc0ebf4b1eed",
+    "url": "/nintendo-eshop/static/media/bg.9ce4b975.png"
+  },
+  {
+    "revision": "b70656730e1ebd56c8e1bfc7ee15fc1f",
+    "url": "/nintendo-eshop/static/media/amiibo-item-bg.b7065673.jpg"
+  },
+  {
+    "revision": "7ab58eeaafbb64188149dabe5bee0f6f",
+    "url": "/nintendo-eshop/static/media/dbfz-item-bg.7ab58eea.jpg"
+  },
+  {
+    "revision": "f9de82755676b307d4b3affa657b18ab",
+    "url": "/nintendo-eshop/static/media/splatoon2-item-bg.f9de8275.jpg"
+  },
+  {
+    "revision": "a4727e4b1efb48e8330490876c2daf70",
+    "url": "/nintendo-eshop/static/media/online_switch_promo_1920x1080.a4727e4b.mp4"
+  },
+  {
+    "revision": "09afc536d3194f5f5a29496fbdfc6ded",
+    "url": "/nintendo-eshop/static/media/bga.09afc536.jpg"
+  },
+  {
+    "revision": "46183950ce8847fd64795fa9adf519c7",
+    "url": "/nintendo-eshop/static/media/bge.46183950.jpg"
+  },
+  {
+    "revision": "1ddb44190acad28108152cb3a3dd40aa",
+    "url": "/nintendo-eshop/static/media/bgf.1ddb4419.jpg"
+  },
+  {
+    "revision": "cff05b44f268d50ab502860bb38b819c",
+    "url": "/nintendo-eshop/static/media/bgi.cff05b44.png"
+  },
+  {
+    "revision": "55c66d7d32501a77558269703072cf6f",
+    "url": "/nintendo-eshop/static/media/bgo.55c66d7d.png"
+  },
+  {
+    "revision": "f9090f26b280bec33ef575d8f9060b96",
+    "url": "/nintendo-eshop/static/media/cappy-mario-footer.f9090f26.png"
+  },
+  {
+    "revision": "99a399a7f7a20ee409e275efd313067e",
+    "url": "/nintendo-eshop/static/media/chara05.99a399a7.png"
+  },
+  {
+    "revision": "e9a7bdfb7c1197b435fbdb9666004a0b",
+    "url": "/nintendo-eshop/static/media/db_fighterz_product_01.e9a7bdfb.jpg"
+  },
+  {
+    "revision": "898f1223c35699754c9b96ea43c0021e",
+    "url": "/nintendo-eshop/static/media/dmc_5_promo_fullsize.898f1223.jpg"
+  },
+  {
+    "revision": "33a59d345bd4aef7807ec1075e6858f5",
+    "url": "/nintendo-eshop/static/media/donkey_kong_country_tropical_freeze_product_01.33a59d34.jpg"
+  },
+  {
+    "revision": "85cbd85adf1434dbce9df5987a11e287",
+    "url": "/nintendo-eshop/static/media/dq-xi-item-bg.85cbd85a.jpg"
+  },
+  {
+    "revision": "dd17f3b8e4f25711dda5021b1177045b",
+    "url": "/nintendo-eshop/static/media/dq_builder_product_01.dd17f3b8.jpg"
+  },
+  {
+    "revision": "a5b5461659a73807c8172b7a9043d082",
+    "url": "/nintendo-eshop/static/media/dqxi-item-bg.a5b54616.png"
+  },
+  {
+    "revision": "09ebac11674cec6dec52e3b180cc9a1e",
+    "url": "/nintendo-eshop/static/media/eshop_logo.09ebac11.png"
+  },
+  {
+    "revision": "190f59296ccac7ccb481d4bc99c94f65",
+    "url": "/nintendo-eshop/static/media/joycon.190f5929.png"
+  },
+  {
+    "revision": "60bfc6a55ba04a05755e8824d230bf02",
+    "url": "/nintendo-eshop/static/media/kirby_star_allies_product_01.60bfc6a5.jpg"
+  },
+  {
+    "revision": "4b9182db2c5be3c056adb5fc1f30090d",
+    "url": "/nintendo-eshop/static/media/labo.4b9182db.jpg"
+  },
+  {
+    "revision": "a886a99b63ae852c558b03c171b24aa3",
+    "url": "/nintendo-eshop/static/media/labo.a886a99b.png"
+  },
+  {
+    "revision": "305dfb7d50d513d636a3c922bce6cd04",
+    "url": "/nintendo-eshop/static/media/lets_go_evoli_product_01.305dfb7d.jpg"
+  },
+  {
+    "revision": "b40a914ed35943b84585b4c64d5868af",
+    "url": "/nintendo-eshop/static/media/lets_go_pikachu_product_01.b40a914e.jpg"
+  },
+  {
+    "revision": "1c4e47a2eff10a109e3dac515ac67b85",
+    "url": "/nintendo-eshop/static/media/logo.1c4e47a2.svg"
+  },
+  {
+    "revision": "633b5a107bb6ca36d44bd00ce1acdb12",
+    "url": "/nintendo-eshop/static/media/mario-item-00.633b5a10.png"
+  },
+  {
+    "revision": "a74c6ff22f2abedd677ba19cbaf7d073",
+    "url": "/nintendo-eshop/static/media/mario_kart_8_product_01.a74c6ff2.jpg"
+  },
+  {
+    "revision": "582d0966e24e8c78f8afc26873986ab9",
+    "url": "/nintendo-eshop/static/media/mario_odyssey_product_01.582d0966.jpg"
+  },
+  {
+    "revision": "10fedaf6350b8e10c1d501cbddb3334c",
+    "url": "/nintendo-eshop/static/media/mario_party_product_01.10fedaf6.jpg"
+  },
+  {
+    "revision": "0d64a712826cf3da7b76cb16152ec272",
+    "url": "/nintendo-eshop/static/media/nin.0d64a712.svg"
+  },
+  {
+    "revision": "3b89f7fa6fe4249cf205dde35c24ba6d",
+    "url": "/nintendo-eshop/static/media/octo.3b89f7fa.jpg"
+  },
+  {
+    "revision": "e1566552cbc5587090291a24d69c0199",
+    "url": "/nintendo-eshop/static/media/octopath_traveler_product_01.e1566552.jpg"
+  },
+  {
+    "revision": "dba53d27e3433838e9987566e9607e2b",
+    "url": "/nintendo-eshop/static/media/odyssey-01.dba53d27.jpg"
+  },
+  {
+    "revision": "acdfaa51ebfd8261a08d54364535e220",
+    "url": "/nintendo-eshop/static/media/old.acdfaa51.svg"
+  },
+  {
+    "revision": "62562c0728ae2fa176471cd3971d87ea",
+    "url": "/nintendo-eshop/static/media/playstation-family-logo-01-eu-12may17.62562c07.svg"
+  },
+  {
+    "revision": "0d961634c6dc280f2983e40e29b778cd",
+    "url": "/nintendo-eshop/static/media/pokego.0d961634.jpg"
+  },
+  {
+    "revision": "05a1b6d3083718c97be3a57d57b30c8d",
+    "url": "/nintendo-eshop/static/media/promo-1.05a1b6d3.png"
+  },
+  {
+    "revision": "80235d1b79079faf1d6d725183211c6e",
+    "url": "/nintendo-eshop/static/media/ssbu_product_01.80235d1b.jpg"
+  },
+  {
+    "revision": "1eb18ea80d1e4388f934191147593f74",
+    "url": "/nintendo-eshop/static/media/switch.1eb18ea8.webp"
+  },
+  {
+    "revision": "cedc0eff5072f687274f2e744a0562d6",
+    "url": "/nintendo-eshop/static/media/test2.cedc0eff.png"
+  },
+  {
+    "revision": "cd205b40feb03327fbc8aa0f0c9f8472",
+    "url": "/nintendo-eshop/static/media/tloz_botw_product_01.cd205b40.jpg"
+  },
+  {
+    "revision": "b3caa586d6e20f49c50ef8a9400ac4d1",
+    "url": "/nintendo-eshop/index.html"
+  }
+];
