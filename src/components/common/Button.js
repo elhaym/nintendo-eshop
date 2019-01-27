fb93c01d3e8084485918699ab6f1bcdb6eb99522
Elhaym <elhaym@users.noreply.github.com>
2019-01-27 21:55:25 +0100

    Revert "First Commit"
    This reverts commit 196b61fdcace8c72a4c5c3bc273bd69f10e21632.

--- src/components/common/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = (props) => {
-    return (
-    <button className={props.style} onClick={props.handleClick} type="button">{props.text}</button>
-    )
-}
-
-export default Button;
--- src/components/layout/CartItems.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const CartItems = (props) => {}
-
-export default CartItems;
